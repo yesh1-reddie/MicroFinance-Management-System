6c6422b87f6f7798b2a587fed67e5fbdb86cb9db
Divya Konduru <divyakonduru029@gmail.com>
2026-07-05 19:09:52 +0530

    status: Implemented Config Server and Eureka Discovery Server

---
+++ platform/config-server/src/test/java/com/shankar/microfinance/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankar.microfinance.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
